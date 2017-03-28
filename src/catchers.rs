f9f2ea6716151f6b02e8c072c2426b0526965d10
zypeh <zypeh.geek@gmail.com>
2017-03-28 12:28:36 +0800

    Catchers separate to a new file: catchers.rs

---
+++ src/catchers.rs
@@ -0,0 +1,41 @@
+use rocket::Request;
+use rocket_contrib::{JSON, Value};
+
+/// 404 Not found catcher
+#[error(404)]
+fn not_found(req: &Request) -> JSON<Value> {
+    let resp = match req.content_type() {
+        // Check if it's application/json typed
+        Some(ref ctxt) if !ctxt.is_json() => {
+            json!({
+                "success": false,
+                "message": format!("Sorry we only supports JSON requests, not '{}'.", ctxt)
+            })
+        },
+        _ => {
+            json!({
+                "success": false,
+                "message": format!("'{}' is an invalid URL.", req.uri())
+            })
+        }
+    };
+    JSON(resp)
+}
+
+/// 400 Bad Request catcher
+#[error(400)]
+fn bad_request() -> JSON<Value> {
+    JSON(json!({
+        "success": false,
+        "message": "The request could not be understood by the server due to malformed syntax."
+    }))
+}
+
+/// 401 Unauthorized
+#[error(401)]
+fn unauthorized() -> JSON<Value> {
+    JSON(json!({
+        "success": false,
+        "message": "Missing or invalid authentication."
+    }))
+}
--- src/main.rs
+++ src/main.rs
@@ -11,41 +11,9 @@ extern crate jsonwebtoken as jwt;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate rocket_contrib;
 
-use rocket::Request;
-use rocket_contrib::{JSON, Value};
-
 mod user;
 mod auth;
-
-/// 404 Not found catcher
-#[error(404)]
-fn not_found(req: &Request) -> JSON<Value> {
-    let resp = match req.content_type() {
-        // Check if it's application/json typed
-        Some(ref ctxt) if !ctxt.is_json() => {
-            json!({
-                "success": false,
-                "message": format!("Sorry we only supports JSON requests, not '{}'.", ctxt)
-            })
-        },
-        _ => {
-            json!({
-                "success": false,
-                "message": format!("'{}' is an invalid URL.", req.uri())
-            })
-        }
-    };
-    JSON(resp)
-}
-
-/// 400 Bad Request catcher
-#[error(400)]
-fn bad_request() -> JSON<Value> {
-    JSON(json!({
-        "success": false,
-        "message": "The request could not be understood by the server due to malformed syntax."
-    }))
-}
+mod catchers;
 
 fn main() {
     rocket::ignite()
@@ -57,8 +25,9 @@ fn main() {
         ])
 
         .catch(errors![
-            not_found,
-            bad_request,
+            catchers::not_found,
+            catchers::bad_request,
+            catchers::unauthorized,
         ])
 
         .launch();
